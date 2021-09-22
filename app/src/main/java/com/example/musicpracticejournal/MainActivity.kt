f1daa6c7ae6b82770bfab10ec9143641fec82f6a
Camilo Cruz <ccamilo_eduardo@hotmail.com>
2021-09-22 11:20:26 -0500

    Add initial navigation setup

--- app/src/main/java/com/example/musicpracticejournal/MainActivity.kt
+++ app/src/main/java/com/example/musicpracticejournal/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.musicpracticejournal
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.musicpracticejournal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,4 +17,23 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
     }
+
+    override fun onStart() {
+        super.onStart()
+        setupBottomNavigationMenu()
+    }
+
+    private fun setupBottomNavigationMenu() {
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!.findNavController()
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.createFragment) {
+                binding.bottomNav.visibility = View.INVISIBLE
+            } else {
+                binding.bottomNav.visibility = View.VISIBLE
+            }
+        }
+    }
+
 }
